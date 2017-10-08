8077b0f56581f2bd4e5d8e82bad420712d69b8a0
leinerf <frenielzabala@gmail.com>
2017-10-07 20:55:27 -0700

    this hopefully works

--- routes/meme.js
+++ routes/meme.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 var Meme = require('../models/meme');
 
+
+
 router.get('/',function(req, res){
 
     Meme.find({},function(err,memes){
