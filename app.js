73503264c2bd8a8a48269bd06f35d3190bd85b02
leinerf <frenielzabala@gmail.com>
2017-10-07 21:45:36 -0700

    change stuff

--- app.js
+++ app.js
@@ -4,14 +4,17 @@ var mongoose = require('mongoose');
 var memeRoute = require('./routes/meme')
 var port =  process.env.PORT || 3000;
 var ip = process.env.IP || null;
-
+var dbuser = process.env.DBUSER;
+var dbpassword = process.env.DBPASSWORD;
 
 //connect to mongoDB
-if(process.env.IP && process.env.PORT){
-    mongoose.connect("mongodb://localhost/memeDB",{useMongoClient:true});
+if(process.env.PORT && process.env.IP){
+    console.log('production');
+    mongoose.connect('mongodb://'+dbuser+':'+dbpassword+'@ds113785.mlab.com:13785/memeshack4',{useMongoClient:true});
 } else {
+    console.log('not production');
     mongoose.connect("mongodb://localhost/memeDB",{useMongoClient:true});
-}
+
 
 //sets up the view engine to ejs and views folder
 app.set('view engine', 'ejs');
